dc92103048bb03c0b691dbe34ed90274771e1660
gabdurr <gabriel.durr@outlook.com>
2022-06-07 18:44:28 -0300

    ..

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     content: ["./src/**/*.tsx"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brand: {
+                    500: "#C5B358",
+                },
+            },
+        },
     },
     plugins: [],
 };

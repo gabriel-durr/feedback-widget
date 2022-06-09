678e3ae77b4fb8bfe3f2de58a772356f741037e4
gabdurr <gabriel.durr@outlook.com>
2022-06-09 11:58:22 -0300

    working styles on the form

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
         extend: {
             colors: {
                 brand: {
+                    300: "#D3c682",
                     500: "#C5B358",
+                },
+                borderRadius: {
+                    md: "4px",
                 },
             },
         },
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
